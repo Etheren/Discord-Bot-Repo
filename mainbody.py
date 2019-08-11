685fcd06ff9b3e65381351ef2eca9470c00e21c4
Callum Griffiths <griffiths.callum16@gmail.com>
2019-08-11 19:52:22 +0100

    Base Files
    included is the token file. please make sure that you have a .gitignore for this file, or you remove your token, otherwise say goodbye to any servers the bot is on.

---
+++ mainbody.py
@@ -0,0 +1,30 @@
+import discord, discordtoken
+from discord.ext import commands
+
+TOKEN = discordtoken.TOKEN
+#You're not getting the bot token THIS easily. Ive already seen one server get
+#Destroyed by it being left here.
+
+description = '''Etherbot'''
+bot = commands.Bot(command_prefix='#', description=description)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+@bot.command()
+async def hello(ctx):
+    """Says world"""
+    await ctx.send("world")
+
+
+@bot.command()
+async def add(ctx, left : int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+bot.run(TOKEN)
